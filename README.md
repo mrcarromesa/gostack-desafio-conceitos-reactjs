522d8a786fc2a32e5400c3751bf41c056ce031a7
Carlos Rodolfo Mengel Santos <mrcarromesa@gmail.com>
2020-06-27 23:05:16 -0300

    Desafio conceitos reactjs

---
+++ README.md
@@ -0,0 +1,2 @@
+# GoStack - Desafio Conceitos ReactJS
+[Desafio 03: Conceitos do ReactJS](https://github.com/Rocketseat/bootcamp-gostack-desafios/tree/master/desafio-conceitos-reactjs)
--- src/App.js
+++ src/App.js
@@ -1,26 +1,57 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 
 import "./styles.css";
+import api from './services/api';
 
 function App() {
+
+  const [repositories, setRepositories] = useState([]);
+
+  useEffect(() => { 
+
+    async function loadRepositories() {
+      const { data } = await api.get('repositories');
+      
+      if (data) {
+        setRepositories(data);
+      }
+    }
+
+    loadRepositories();
+
+  }, []);
+
   async function handleAddRepository() {
-    // TODO
+    const { data } = await api.post('repositories',{
+      title: `title ${new Date().getTime()}`,
+      url: 'http://localhost',
+      techs: ['Tech1'],
+    });
+
+    setRepositories([...repositories, data]);
   }
 
   async function handleRemoveRepository(id) {
-    // TODO
+    try {
+      await api.delete(`repositories/${id}`);
+      const updatedRepository = repositories.filter((r) => r.id !== id);
+      setRepositories(updatedRepository);
+    } catch (error) {
+      alert('Algo deu errado ao tentar remover o item');
+    }
   }
 
   return (
     <div>
       <ul data-testid="repository-list">
-        <li>
-          Repositório 1
-
-          <button onClick={() => handleRemoveRepository(1)}>
-            Remover
-          </button>
-        </li>
+        {repositories.map((r) => (
+          <li key={r.id}>
+            {r.title}
+            <button onClick={() => handleRemoveRepository(r.id)}>
+              Remover
+            </button>
+          </li>
+        ))}
       </ul>
 
       <button onClick={handleAddRepository}>Adicionar</button>
